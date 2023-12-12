136594e2256cf50717ea6b907e978d96f84b59a0
Rashid Saleem <mrashidcit@gmail.com>
2023-12-12 07:43:20 +0500

    Splash Screen UI have been completed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.rashidsaleem.eventbookingapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.rashidsaleem.eventbookingapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -53,15 +53,22 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+
+    // Compose
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
+
+    // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Compose-Testing
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
---
+++ app/src/main/java/com/rashidsaleem/eventbookingapp/presentation/splash/SplashScreen.kt
@@ -0,0 +1,98 @@
+package com.rashidsaleem.eventbookingapp.presentation.splash
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.rashidsaleem.eventbookingapp.R
+import com.rashidsaleem.eventbookingapp.ui.theme.EventBookingAppTheme
+
+@Composable
+fun SplashScreen() {
+    ConstraintLayout(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        val (logoAndTextContainer, topRightCircle,
+            bottomLeftCircle, bottomRightCircle) = createRefs()
+
+        // Top-Right Circle
+        Image(
+            modifier = Modifier
+                .constrainAs(topRightCircle) {
+                    top.linkTo(parent.top)
+                    end.linkTo(parent.end)
+                },
+            painter = painterResource(id = R.drawable.img_top_right_background_circle),
+            contentDescription = null
+        )
+
+        // Bottom-Left Circle
+        Image(
+            modifier = Modifier
+                .constrainAs(bottomLeftCircle) {
+                    start.linkTo(parent.start)
+                    bottom.linkTo(parent.bottom)
+                },
+            painter = painterResource(id = R.drawable.img_bottom_left_background_circle),
+            contentDescription = null
+        )
+
+        // Bottom-Right Circle
+        Image(
+            modifier = Modifier
+                .constrainAs(bottomRightCircle) {
+                    end.linkTo(parent.end)
+                    bottom.linkTo(parent.bottom)
+                },
+            painter = painterResource(id = R.drawable.img_bottom_right_background_circle),
+            contentDescription = null
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .constrainAs(logoAndTextContainer) {
+                    this.centerVerticallyTo(parent)
+                }
+            ,
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            Image(
+                modifier = Modifier
+                    .padding(bottom = 7.5.dp),
+                painter = painterResource(id = R.drawable.app_logo),
+                contentDescription = null,
+                )
+            Spacer(modifier = Modifier.width(10.dp))
+            Image(
+                painter = painterResource(id = R.drawable.img_venthub_text),
+                contentDescription = null,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SplashScreenPreview() {
+    EventBookingAppTheme {
+        Surface {
+            SplashScreen()
+        }
+    }
+}
+
